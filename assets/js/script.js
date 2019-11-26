8c041d370127a4dcb37e5b973e7abcaecd382ad9
DevAngelo98 <ainglima07@gmail.com>
2019-11-26 18:53:39 +0100

    Serietv

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,13 +22,15 @@ $(document).ready(function(){
 
   //Funzione "ricerca" con la chiamata ajax da eseguire
   function ricerca (nome){
+
+    //Chiamata per i film
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie?api_key=66aeb90ff00ebee2e50dd67451722ef8&query="+ nome,
       method: "GET",
       success: function(datoRicevuto){
         $(".form-control").val("");
         var arrayRicevuto = datoRicevuto.results;
-        var source = $("#template").html();
+        var source = $("#template-serie").html();
         var template = Handlebars.compile(source);
         arrayRicevuto.forEach(element => {
           // stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';
@@ -41,12 +43,35 @@ $(document).ready(function(){
             stars: stelle(votoFinale)
           };
           var html = template(context);
-          // console.log(html);
-          // stelle(votoFinale);
           $(".lista").append(html);
         });
       }
     });  
+
+    //Chiamata per le serie tv
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/tv?api_key=66aeb90ff00ebee2e50dd67451722ef8&query="+ nome,
+      method: "GET",
+      success: function(serie){
+        $(".form-control").val("");
+        var arraySerie = serie.results;
+        var source = $("#template-film").html();
+        var template = Handlebars.compile(source);
+        arraySerie.forEach(element => {
+          // stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';
+          var votoFinale = conversioneVoto(element.vote_average);
+          var context = {
+            titolo: element.name, 
+            titoloOriginale: element.original_name, 
+            lingua: bandiera(element.original_language), 
+            voto: votoFinale,
+            stars: stelle(votoFinale)
+          };
+          var html = template(context);
+          $(".lista").append(html);
+        });
+      }
+    });
   }
 
   
