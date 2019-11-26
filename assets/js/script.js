99b65d9766d1bff75fc12e68acc0be9ccfcf0aec
DevAngelo98 <ainglima07@gmail.com>
2019-11-26 20:33:16 +0100

    Img

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,13 @@ $(document).ready(function(){
   //Al click del bottone mi ritorna il valore dell'input e pulisco (se già presente) il contenuto di ".lista"
   //richiamo tramite la funzione "ricerca" la chiamata ajax alla quale passo come query il nome da ricercare
   $("#button-addon2").click(function(){
-    $(".lista *").remove();
+    $(".lista .film > ul").remove();
+    $(".listaserie .serie > ul").remove();
     var valore = $(".form-control").val();
     ricerca(valore);
   });
 
-  //Funzione "ricerca" con la chiamata ajax da eseguire
+  //Funzione "ricerca" con le chiamata ajax da eseguire
   function ricerca (nome){
 
     //Chiamata per i film
@@ -30,12 +31,13 @@ $(document).ready(function(){
       success: function(datoRicevuto){
         $(".form-control").val("");
         var arrayRicevuto = datoRicevuto.results;
-        var source = $("#template-serie").html();
+        console.log(arrayRicevuto);
+        var source = $("#template-film").html();
         var template = Handlebars.compile(source);
         arrayRicevuto.forEach(element => {
-          // stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';
           var votoFinale = conversioneVoto(element.vote_average);
           var context = {
+            imgpassata: 'https://image.tmdb.org/t/p/w342/'+element.backdrop_path,
             titolo: element.title, 
             titoloOriginale: element.original_title, 
             lingua: bandiera(element.original_language), 
@@ -43,7 +45,7 @@ $(document).ready(function(){
             stars: stelle(votoFinale)
           };
           var html = template(context);
-          $(".lista").append(html);
+          $(".lista .film").append(html);
         });
       }
     });  
@@ -55,12 +57,14 @@ $(document).ready(function(){
       success: function(serie){
         $(".form-control").val("");
         var arraySerie = serie.results;
-        var source = $("#template-film").html();
+        console.log(arraySerie);
+        
+        var source = $("#template-serie").html();
         var template = Handlebars.compile(source);
         arraySerie.forEach(element => {
-          // stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';
           var votoFinale = conversioneVoto(element.vote_average);
           var context = {
+            imgpassata: 'https://image.tmdb.org/t/p/w342/'+element.backdrop_path,
             titolo: element.name, 
             titoloOriginale: element.original_name, 
             lingua: bandiera(element.original_language), 
@@ -68,7 +72,7 @@ $(document).ready(function(){
             stars: stelle(votoFinale)
           };
           var html = template(context);
-          $(".lista").append(html);
+          $(".listaserie .serie").append(html);
         });
       }
     });
