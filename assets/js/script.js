895b8a1960c3f81e362668b0855c8f84c0088f35
DevAngelo98 <ainglima07@gmail.com>
2019-11-27 12:24:20 +0100

    Descrizione

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
   //Cambio stile bandiera all'hover
-  $(".container").on("mouseenter", ".flaghover", function(){
+  $(".info-film-serie").on("mouseenter", ".flaghover", function(){
     $(this).addClass("none");
     $(this).parent().find(".flagnohover").removeClass("none");
+    console.log("OK");
+    
   });
 
-  $(".container").on("mouseleave", ".flagnohover", function(){
+  $(".info-film-serie").on("mouseleave", ".flagnohover", function(){
     $(this).addClass("none");
-    // // $(this).addClass("block");
     $(this).parent().find(".flaghover").removeClass("none");
   });
 
@@ -31,8 +32,7 @@ $(document).ready(function(){
       success: function(datoRicevuto){
         $(".form-control").val("");
         var arrayRicevuto = datoRicevuto.results;
-        console.log(arrayRicevuto);
-        var source = $("#template-film").html();
+        var source = $("#template-serie-film").html();
         var template = Handlebars.compile(source);
         arrayRicevuto.forEach(element => {
           var votoFinale = conversioneVoto(element.vote_average);
@@ -42,8 +42,10 @@ $(document).ready(function(){
             titoloOriginale: element.original_title, 
             lingua: bandiera(element.original_language), 
             voto: votoFinale,
-            stars: stelle(votoFinale)
+            stars: stelle(votoFinale),
+            descrizione: element.overview
           };
+          controlloImg(element.backdrop_path, context);
           var html = template(context);
           $(".lista .film").append(html);
         });
@@ -57,10 +59,10 @@ $(document).ready(function(){
       success: function(serie){
         $(".form-control").val("");
         var arraySerie = serie.results;
+        var source = $("#template-serie-film").html();
+        var template = Handlebars.compile(source);
         console.log(arraySerie);
         
-        var source = $("#template-serie").html();
-        var template = Handlebars.compile(source);
         arraySerie.forEach(element => {
           var votoFinale = conversioneVoto(element.vote_average);
           var context = {
@@ -69,8 +71,10 @@ $(document).ready(function(){
             titoloOriginale: element.original_name, 
             lingua: bandiera(element.original_language), 
             voto: votoFinale,
-            stars: stelle(votoFinale)
+            stars: stelle(votoFinale),
+            descrizione: element.overview
           };
+          controlloImg(element.backdrop_path, context);
           var html = template(context);
           $(".listaserie .serie").append(html);
         });
@@ -78,6 +82,11 @@ $(document).ready(function(){
     });
   }
 
+  function controlloImg (val, context){
+    if(val==null){
+      context.imgnulla = "flexmy";
+    }
+  }
   
   function conversioneVoto (voto){
     var votoDivisoPerDue = voto/2;
@@ -93,27 +102,54 @@ $(document).ready(function(){
       linguaUpper = "CZ";
     } else if (linguaUpper=="JA"){
       linguaUpper = "JP";
+    } else if (linguaUpper=="HE"){
+      linguaUpper = "IL";
     }
     return linguaUpper;
   }
 
 
   function stelle(voto){
-
     var init="";
-
     for(j=0; j<voto; j++){
-      stella = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="black" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" class=""></path></svg>';
+      stella = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="yellow" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" class=""></path></svg>';
       init+=stella;
     }
-
-    var stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';  
-    
+    var stella = '<svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18 fa-3x"><path fill="yellow" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z" class=""></path></svg>';  
     for(var i=0; i<5-voto; i++){
       init+=stella;
     }
-
     return init;
-  
   }
+
+  //Hover sul film/serie e mostro div info
+  
+  $(".container").on("mouseenter", "ul .img", function(){
+    $(this).find(".info").addClass("flex");
+  });
+
+  $(".container").on("mouseleave", "ul .img", function(){
+    $(this).find(".info").removeClass("flex");  
+  });
+
+  //Clone delle informazioni
+  $(".container").on("click", ".img", function(){
+    var copia = $(this).parent().parent().clone().addClass("copiato");
+    $(".info-film-serie").html(copia);
+    $(".info-film-serie").removeClass("none");
+    $(".info-film-serie").addClass("flex");
+    $(".info-film-serie .descr").removeClass("none");
+    $(".info-film-serie .descr").addClass("flex");
+    $(".info-film-serie .descrizione").removeClass("none");
+    $(".container.lista").addClass("none");
+    $(".container.listaserie").addClass("none");
+  });
+
+  //Ripristino ricerca
+  $("body").on("click", ".closedescrizione", function(){
+    $(".info-film-serie").removeClass("flex");
+    $(".info-film-serie").addClass("none");
+    $(".container.lista").removeClass("none");
+    $(".container.listaserie").removeClass("none");
+  });
 });
